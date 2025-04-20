44feb1b678449a3db4e852b5b6e4715390dec953
Azechum30 <azechum21@gmail.com>
2025-04-20 16:33:11 +0000

    wrangler.jsonc

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 const nextConfig: NextConfig = {
   turbopack: {},
@@ -7,6 +8,10 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
 };
+
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
 
 export default withSentryConfig(nextConfig, {
   // For all available options, see:
