9cef5a82baeaf360364de6a7633bd8a227cb9736
Azechum30 <azechum21@gmail.com>
2025-04-20 14:47:53 +0000

    made correcttions to env variables

--- src/app/(public)/password-reset/page.tsx
+++ src/app/(public)/password-reset/page.tsx
@@ -1,9 +1,10 @@
-import ResetPasswordForm from "@/components/customComponents/ResetPasswordForm"
+import ResetPasswordForm from "@/components/customComponents/ResetPasswordForm";
 
+export const dynamic = "force-dynamic";
 export default function PasswordResetPage() {
-	return (
-		<div className='w-full max-w-md mx-auto'>
-			<ResetPasswordForm />
-		</div>
-	)
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <ResetPasswordForm />
+    </div>
+  );
 }
