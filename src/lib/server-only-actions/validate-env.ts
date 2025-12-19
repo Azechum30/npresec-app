6d54ba581e723db62d9d5bd7c5f35e32dc8a7048
Azechum30 <azechum21@gmail.com>
2025-12-19 17:59:30 +0000

    remove an unused env variable

--- src/lib/server-only-actions/validate-env.ts
+++ src/lib/server-only-actions/validate-env.ts
@@ -11,7 +11,6 @@ const envSchema = z.object({
   ADMIN_USER_PASSWORD: z.string().min(1),
   UPSTASH_REDIS_REST_URL: z.string().url(),
   UPSTASH_REDIS_REST_TOKEN: z.string().min(1),
-  REDIS_URL: z.string().url(),
   QSTASH_TOKEN: z.string().min(1),
   QSTASH_URL: z.string().url(),
   QSTASH_CURRENT_SIGNING_KEY: z.string().min(1),
